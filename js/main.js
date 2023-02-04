f6148eb631965c90a6870978ddf854e7b59cdf3d
Camila Gatley <75987820+camila-gatley@users.noreply.github.com>
2023-02-04 18:23:24 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 const apiKey = 'f169182e855b51c0d4ec8b29896c2344';
 const weatherLocation = 'Buenos Aires, Argentina';
-const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=Buenos%20Aires,Argentina&appid=${apiKey}`;
+//const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=Buenos%20Aires,Argentina&appid=${apiKey}`;
 
-fetch(weatherUrl)
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=Buenos%20Aires,Argentina&appid=${apiKey}`)
 .then(response => response.json())
 .then(data => {
     const city = data.name;
